b74384850340cc0f6099f9e2bf2ffad368777016
pavan karale <pawankarale007@gmail.com>
2024-07-17 11:30:13 +0530

    first commit

---
+++ A1vg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class A1vg{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int Avg = (A+B+C)/3;
+        System.out.println("Average of the num is ="+Avg);
+    }
+}
+
+ 
